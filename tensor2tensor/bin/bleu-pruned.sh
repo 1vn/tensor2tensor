1e564060f0a5598e69c314e1df8b966e55b6bf7e
Ivan Zhang <ivan@ivanzhang.ca>
2019-05-30 09:41:35 -0400

    add passing in hparam and tpu

--- tensor2tensor/bin/bleu-pruned.sh
+++ tensor2tensor/bin/bleu-pruned.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=""
 set -eu
 
 
-h=transformer_xtreme_dropout_botk_99_100000_keep3
+h=$1
 o=$h
 python tensor2tensor/tensor2tensor/bin/t2t_prune_save.py --data_dir=gs://for-ai/t2t_data --problem=translate_ende_wmt32k --model=transformer --hparams_set=$h --output_dir=gs://for-ai/runs/a1vn/$o --pruning_params_set=transformer_weight --worker_gpu=0
 
@@ -16,7 +16,7 @@ do
   --problem=translate_ende_wmt32k \
   --model=transformer \
   --hparams_set=$h \
-  --worker_gpu=0 --use_tpu=True --cloud_tpu_name=ivan-1  --decode_to_file="gs://for-ai/runs/a1vn/$o/prune_$i/translation.en" --decode_hparams="beam_size=$BEAM_SIZE,alpha=$ALPHA" \
+  --worker_gpu=0 --use_tpu=True --cloud_tpu_name=$2  --decode_to_file="gs://for-ai/runs/a1vn/$o/prune_$i/translation.en" --decode_hparams="beam_size=$BEAM_SIZE,alpha=$ALPHA" \
   --output_dir="gs://for-ai/runs/a1vn/$o/prune_$i"
 
 done
